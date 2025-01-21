0b5e9623be997de9d33633f43b1932fcee428b5b
Geoff Golda <gcgolda@gmail.com>
2025-01-20 20:56:22 -0800

    nojekyll

--- src/App.js
+++ src/App.js
@@ -3,20 +3,36 @@ import "./App.css";
 
 const Portfolio = () => {
   const [transform, setTransform] = useState("scale(1) translateY(0px)");
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
+      // Only run scroll handler if on mobile
+      if (!isMobile) {
+        setTransform("none");
+        return;
+      }
+
       const scrollPosition = window.scrollY;
 
       // Define the gap after which scaling should happen
-      const gapThreshold = 200; // When scrolling past 300px
+      const gapThreshold = 190; // When scrolling past 300px
       const maxScale = 1; // Maximum scale value (start size)
       const minScale = 0.6; // Minimum scale value (end size)
 
       // Calculate scale based on scroll position
       let scaleValue = Math.max(
         minScale,
-        Math.min(maxScale - (scrollPosition - gapThreshold) / 200, maxScale)
+        Math.min(maxScale - (scrollPosition - gapThreshold) / 190, maxScale)
       );
 
       console.log("scrollPosition", scrollPosition);
